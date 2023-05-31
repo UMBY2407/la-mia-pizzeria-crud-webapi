d2acd523db30f6723ebd2bf3d08c1d7885d63212
UMBY2407 <umby2407@gmail.com>
2023-05-31 15:45:05 +0200

    Added API Pizza controller

---
+++ LaMiaPizzeria/Controllers/API/PizzaAPIController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using LaMiaPizzeria.Models;
+using LaMiaPizzeria.Database;
+
+namespace LaMiaPizzeria.Controllers.API
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class PizzaAPIController : Controller
+	{
+		public IActionResult GetPizzas()
+		{
+			using(PizzeriaContext db = new PizzeriaContext())
+			{
+				List<Pizza> pizzas = db.Pizzas.ToList();
+				return Ok(pizzas);
+			}
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult SearchById(int id)
+		{
+			using (PizzeriaContext db = new PizzeriaContext)
+			{
+				Pizza? pizzaToSearch = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+				if(pizzaToSearch != null)
+				{
+					return Ok(pizzaToSearch);
+				}
+				else
+				{
+					return NotFound();
+				}
+			}
+		}
+
+		[HttpGet]
+		public IActionResult SearchByName (string name)
+		{
+			using(PizzeriaContext db = new PizzeriaContext())
+			{
+				Pizza? pizzaToSearch = db.Pizzas.Where(pizza => pizza.Name.Contains(name)).FirstOrDefault();
+
+				if(pizzaToSearch != null)
+				{
+					return Ok(pizzaToSearch);
+				} else
+				{
+					return NotFound();
+				}
+			}
+		}
+
+		[HttpPost]
+		public IActionResult Create([FromBody] Pizza pizza)
+		{
+			if(!ModelState.IsValid)
+			{
+				return BadRequest(); 
+			} else
+			{
+				using(PizzeriaContext db = new PizzeriaContext)
+				{
+					db.Pizzas.Add(pizza);
+					db.SaveChanges();
+
+					return Ok();
+				}
+			}
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			using(PizzeriaContext db = new PizzeriaContext())
+			{
+				Pizza? pizzaToDelete = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+				if(pizzaToDelete != null)
+				{
+					db.Remove(pizzaToDelete);
+					db.SaveChanges();
+					return Ok();
+				}else
+				{
+					return NotFound("Non ho trovato la pizza da eliminare");
+				}
+			}
+		}
+	}
+}
