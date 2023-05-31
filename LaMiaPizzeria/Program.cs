200f730f0f5be79c3c568ceaff0d05b458a3fc16
UMBY2407 <umby2407@gmail.com>
2023-05-31 17:37:52 +0200

    modified program.cs Home Default View

--- LaMiaPizzeria/Program.cs
+++ LaMiaPizzeria/Program.cs
@@ -18,10 +18,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseCors(
+  options => options.WithOrigins("*").AllowAnyMethod().AllowAnyHeader()
+	  );
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Pizza}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
